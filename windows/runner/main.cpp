4d6dc8d677f187c6bbb13256aae8b8e765753013
NikolayNIK <nikitin.nikolay@bk.ru>
2023-01-08 15:00:43 +0800

    Update platform projects

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280 + 16, 720 + 39);
-  if (!window.Create(L"material_table_view_demo", origin, size)) {
+  Win32Window::Size size(1280, 720);
+  if (!window.CreateAndShow(L"example", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
